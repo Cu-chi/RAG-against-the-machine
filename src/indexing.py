5a2b93fe79b1c84072d419d5b8e94d8a9db19b53
Cu-chi <110144918+Cu-chi@users.noreply.github.com>
2026-08-12 09:02:48 +0200

    fix(indexing): save metadata correctly

--- src/indexing.py
+++ src/indexing.py
@@ -6,14 +6,15 @@ from pathlib import Path
 
 def store_chunks(chunks: List[Document], output: str = "data/processed/"):
     corpus = [chunk.page_content for chunk in chunks]
-    metadata_corpus = [dict(chunk) for chunk in chunks]
+    metadata_corpus = [{"page_content": chunk.page_content,
+                        "metadata": chunk.metadata} for chunk in chunks]
     corpus_tokens = bm25s.tokenize(corpus)
 
     retriever = bm25s.BM25(corpus=metadata_corpus)
     retriever.index(corpus_tokens)
 
     Path(output).mkdir(parents=True, exist_ok=True)
-    retriever.save(f"{output}/bm25_model", corpus=corpus)
+    retriever.save(f"{output}/bm25_model")
 
 
 def search_query(query: str, k: int = 5,
